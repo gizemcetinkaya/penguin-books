e4914e409cafd7d2ca6859d1b07b5bcf99bb61bb
Gizem Selli <cetinkayagizem@gmail.com>
2023-04-13 11:49:00 +0200

    LandingPage and Search pages created. React router added. SearchBar component created.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Layout from "./components/Layout";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <Layout>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/search" element={<Search />} />
         </Routes>
     </Layout>
   );
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,10 +1,13 @@
-function LandingPage() {
+import SearchBar from "../components/SearchBar";
+
+const LandingPage = () => {
     return (
-        <div className="container mx-auto py-10 h-64 px-6">
+        <div className="container mx-auto py-10 h-64 px-6 space-y-12 my-40">
             <div className="text-center">
                 <h1 className="text-[5rem] font-bold text-[color:#15233a] leading-none">BOOKS</h1>
                 <p className="text-[color:#15233a] tracking-[.35em] font-medium">PENGUIN RANDOM HOUSE</p>
             </div>
+            <SearchBar />
         </div>
     );
 }
